bfc520481c9022b4bcf9dcd3add5ddf4e9bc4342
fahadhossain24 <fahadhossaim24@gmail.com>
2025-10-19 17:34:07 +0600

    use s3 bucket for blob storage

--- src/app/modules/heroModule/hero.routes.ts
+++ src/app/modules/heroModule/hero.routes.ts
@@ -7,7 +7,7 @@ import { ENUM_USER_ROLE } from '../../../enums/user';
 const heroRouter = express.Router();
 
 // Route to create or update About Us content (only accessible to admin or super-admin)
-heroRouter.put('/create', authentication(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN), authorization('super-admin', 'admin'), heroControllers.createHero);
+heroRouter.put('/create', authentication(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN), heroControllers.createHero);
 heroRouter.get('/retrieve', heroControllers.getHero);
 
 export default heroRouter;
